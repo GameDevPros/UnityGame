22dbb32ba6ea9d068c9275f67ed5c5c3a6b4f3c7
Tibor Stachoske <tibor.stachoske@gmail.com>
2019-11-17 16:38:24 +0100

    click to move added

---
+++ Assets/Scripts/Player/ClickToMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickToMove : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+    bool isRunning;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(ray, out hit, 100))
+            {
+                navMeshAgent.destination = hit.point;
+            }
+        }
+
+        if(navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+        {
+            isRunning = false;
+        }
+        else
+        {
+            isRunning = true;
+        }
+    }
+}
