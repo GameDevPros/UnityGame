d494d337fda6ee89a1c702df42b7c183944b390f
xTiborius <tibor.stachoske@gmail.com>
2019-11-17 11:37:43 +0100

    Test Playermovement

---
+++ Assets/Scripts/Player/PlayerMovement_2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement_2 : MonoBehaviour {
+
+	public float moveSpeed = 50;
+
+	Rigidbody rb;
+
+    Camera viewCamera;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        viewCamera = Camera.main;
+    }
+	
+	void FixedUpdate () 
+	{
+		float hor = Input.GetAxisRaw("Horizontal");
+        float var = Input.GetAxisRaw("Vertical");    
+		var rigidbody = GetComponent<Rigidbody>();
+
+		rigidbody.velocity = new Vector3(hor,0, var).normalized * moveSpeed;
+
+		float angleRad = Mathf.Atan2 (hor, var);
+		float angleDeg = angleRad * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(0,angleDeg,0);
+	}
+}
